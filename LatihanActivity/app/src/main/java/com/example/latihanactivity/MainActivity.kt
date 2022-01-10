2e2e3e8de42c4dac2d1618a6bf927d77c53ea4c6
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-10 07:43:25 +0700

    added main activity codes

--- LatihanActivity/app/src/main/java/com/example/latihanactivity/MainActivity.kt
+++ LatihanActivity/app/src/main/java/com/example/latihanactivity/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.latihanactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() , View.OnClickListener{
     private lateinit var edtWidth: EditText
@@ -25,7 +27,58 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
         tvResult = findViewById(R.id.tv_result)
     }
 
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+    override fun onClick(view: View?) {
+        if(view?.id == R.id.btn_calculate){
+            var inputLen = edtLength.text.toString().trim()
+            var inputWidth = edtWidth.text.toString().trim()
+            var inputHeight = edtHeight.text.toString().trim()
+
+            var isEmptyFields = false
+            var isInvalidDouble = false
+
+            if (TextUtils.isEmpty(inputLen)){
+                isEmptyFields = true
+                edtLength.error = "Field ini tidak boleh kosong"
+            }
+            if (TextUtils.isEmpty(inputWidth)){
+                isEmptyFields = true
+                edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            if (TextUtils.isEmpty(inputHeight)){
+                isEmptyFields = true
+                edtHeight.error = "Field ini tidak boleh kosong"
+            }
+
+            var length = convertToDouble(inputLen)
+            var width = convertToDouble(inputWidth)
+            var height = convertToDouble(inputHeight)
+
+            if (length == null){
+                isInvalidDouble = true
+                edtLength.error = "nilai tidak valid"
+            }
+            if (width == null){
+                isInvalidDouble = true
+                edtWidth.error = "nilai tidak valid"
+            }
+
+            if (height == null){
+                isInvalidDouble = true
+                edtHeight.error = "nilai tidak valid"
+            }
+
+            if(!isEmptyFields && !isInvalidDouble){
+                var volume = height!! * length!! * length
+                tvResult.text = volume.toString()
+            }
+        }
+    }
+
+    private fun convertToDouble(str: String):Double?{
+        return try {
+            str.toDouble()
+        }catch (e : NumberFormatException){
+            null
+        }
     }
 }
