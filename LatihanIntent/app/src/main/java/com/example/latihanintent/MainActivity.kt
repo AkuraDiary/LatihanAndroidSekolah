a4ae605029741cf602c8cc83fb0c208b8db44f19
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-04 14:30:09 +0700

    added button on click listener, but im confused with the binding

--- LatihanIntent/app/src/main/java/com/example/latihanintent/MainActivity.kt
+++ LatihanIntent/app/src/main/java/com/example/latihanintent/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.latihanintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.latihanintent.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btnMoveActivity = binding.btnMoveActivity
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when{
+            // TODO:  
+        }
     }
 }
