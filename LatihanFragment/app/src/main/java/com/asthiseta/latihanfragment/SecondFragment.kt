dffb0d1b32ea7790f55f112c45e3b067cc4185b1
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-08 08:24:41 +0700

    added code in second fragment.kt

--- LatihanFragment/app/src/main/java/com/asthiseta/latihanfragment/SecondFragment.kt
+++ LatihanFragment/app/src/main/java/com/asthiseta/latihanfragment/SecondFragment.kt
@@ -5,7 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 class SecondFragment : Fragment() {
-
+    private var myViewModel: MyViewModel? = null
+    private var txtName: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        myViewModel = ViewModelProviders.of(requireActivity()).get(MyViewModel::class.java)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second,
+            container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        txtName = view.findViewById(R.id.textViewName)
+        myViewModel!!.name.observe(requireActivity(),
+            Observer { s -> txtName!!.text = s })
+    }
+    companion object {
+        fun newInstance(): SecondFragment {
+            return SecondFragment()
+        }
+    }
 }
