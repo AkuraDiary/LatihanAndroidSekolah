e9f19064071e93775ba4e9a5ad78c7052a7323b2
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-11 08:34:35 +0700

    added maven repository maven.google.com

--- PraktikumFragment/app/src/main/java/com/asthiseta/praktikumfragment/SecondFragment.kt
+++ PraktikumFragment/app/src/main/java/com/asthiseta/praktikumfragment/SecondFragment.kt
@@ -20,7 +20,7 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_second,
+        return inflater.inflate(R.layout.fragment_profile,
             container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
