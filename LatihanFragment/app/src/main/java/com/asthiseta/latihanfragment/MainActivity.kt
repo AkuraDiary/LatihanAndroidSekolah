50cc7ef9328c1f4c230c0b9dc4da990ab6eeae28
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-11 07:25:16 +0700

    bismillah praktikum fragment

--- LatihanFragment/app/src/main/java/com/asthiseta/latihanfragment/MainActivity.kt
+++ LatihanFragment/app/src/main/java/com/asthiseta/latihanfragment/MainActivity.kt
@@ -2,10 +2,17 @@ package com.asthiseta.latihanfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view_pager = findViewById<ViewPager>(R.id.view_pager)
+        val tabs = findViewById<TabLayout>(R.id.tabs)
+        view_pager.adapter = ViewPagerAdapter( this, supportFragmentManager)
+        tabs.setupWithViewPager(view_pager)
     }
 }
