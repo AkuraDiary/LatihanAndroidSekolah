bae5b519b70f4554471b8f4fc985d2c535ced948
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-12 12:40:23 +0700

    added alamat & telp var in view model

--- PraktikumFragment/app/src/main/java/com/asthiseta/praktikumfragment/MyViewModel.kt
+++ PraktikumFragment/app/src/main/java/com/asthiseta/praktikumfragment/MyViewModel.kt
@@ -7,8 +7,26 @@ import androidx.lifecycle.ViewModel
 class MyViewModel : ViewModel() {
 
     private val mName = MutableLiveData<String>()
+    private val mALamat = MutableLiveData<String>()
+    private val mTelp = MutableLiveData<String>()
+
     val name: LiveData<String>
         get() = mName
+
+    val alamat: LiveData<String>
+        get() = mALamat
+
+    val telp: LiveData<String>
+        get() = mTelp
+
+    fun setAlamat(alamat: String) {
+        mALamat.value = alamat
+    }
+
+    fun setTelp(telp: String) {
+        mTelp.value = telp
+    }
+
     fun setName(name: String) {
         mName.value = name
     }
