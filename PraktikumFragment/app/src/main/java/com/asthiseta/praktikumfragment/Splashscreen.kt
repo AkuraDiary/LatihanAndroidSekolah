146b8d6814f165f213762e1bcf592d69b385511e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-13 11:12:04 +0700

    yey splashscreen

--- PraktikumFragment/app/src/main/java/com/asthiseta/praktikumfragment/Splashscreen.kt
+++ PraktikumFragment/app/src/main/java/com/asthiseta/praktikumfragment/Splashscreen.kt
@@ -1,6 +1,7 @@
 package com.asthiseta.praktikumfragment
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,5 +10,23 @@ class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        supportActionBar?.hide()
+
+        //courutine thread
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    val intent = Intent(this@Splashscreen, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+        background.start()
     }
 }
