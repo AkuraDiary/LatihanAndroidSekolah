7780a9cf7c5f6fee7521391166ae34f775f586e0
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-13 10:07:19 +0700

    added splashscreen activity

---
+++ PraktikumFragment/app/src/main/java/com/asthiseta/praktikumfragment/Splashscreen.kt
@@ -0,0 +1,13 @@
+package com.asthiseta.praktikumfragment
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class Splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
