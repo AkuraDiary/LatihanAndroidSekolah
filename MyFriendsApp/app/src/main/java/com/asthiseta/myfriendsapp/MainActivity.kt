bc0684ccd4c2b34f07bd68128ac3d40e58f50c4b
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-19 06:11:53 +0700

    added helper function in main activity

--- MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/MainActivity.kt
+++ MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.asthiseta.myfriendsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tampilMyFriendsFragment()
+    }
+
+    private fun gantiFragment(
+        fragmentManager: FragmentManager,
+        fragment: Fragment, frameId: Int
+    ) {
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(frameId, fragment)
+        transaction.commit()
+    }
+
+    private fun tampilMyFriendsFragment() {
+        gantiFragment(
+            supportFragmentManager,
+            MyFriendsFragment.newInstance(), R.id.contentFrame
+        )
+    }
+
+    private fun tampilMyFriendsAddFragment() {
+        gantiFragment(
+            supportFragmentManager,
+            MyFriendsAddFragment.newInstance(), R.id.contentFrame)
     }
 }
