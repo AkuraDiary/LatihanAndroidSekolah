ba9d1277071e0a7a78e213aff0ba94e795e98c6c
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-18 08:44:36 +0700

    added inner view holder class

--- MovieProject/app/src/main/java/com/smktelkommlg/movieproject/MovieAdapter.kt
+++ MovieProject/app/src/main/java/com/smktelkommlg/movieproject/MovieAdapter.kt
@@ -1,12 +1,17 @@
 package com.smktelkommlg.movieproject
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MovieAdapter : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        LayoutInflater.from(parent.context).inflate(R.layout.movie_adapter, parent, false)
+        return ViewHolder(parent)
     }
 
     override fun getItemCount(): Int {
@@ -17,7 +22,10 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val img_poster : ImageView = itemView.findViewById(R.id.img_poster)
+        val title_view : TextView = itemView.findViewById(R.id.title_view)
+        val release_view : TextView = itemView.findViewById(R.id.realesed_view)
+        val genre_view : TextView = itemView.findViewById(R.id.genre_view)
     }
 }
