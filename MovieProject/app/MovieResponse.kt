59db592ae6235fdaa4f5f7227c99f1fe7f64ff97
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-18 08:06:15 +0700

    added movie response

---
+++ MovieProject/app/MovieResponse.kt
@@ -0,0 +1,16 @@
+@Parcelize
+data class MovieResponse(
+	val response: String? = null,
+	val totalResults: String? = null,
+	val search: List<SearchItem?>? = null
+) : Parcelable
+
+@Parcelize
+data class SearchItem(
+	val type: String? = null,
+	val year: String? = null,
+	val imdbID: String? = null,
+	val poster: String? = null,
+	val title: String? = null
+) : Parcelable
+
