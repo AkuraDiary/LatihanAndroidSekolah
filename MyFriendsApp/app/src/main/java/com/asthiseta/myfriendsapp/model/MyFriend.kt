8a0deeedcc33327ee2d9c64b8640be5ddf8ca862
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-22 00:25:04 +0700

    added model

---
+++ MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/model/MyFriend.kt
@@ -0,0 +1,9 @@
+package com.asthiseta.myfriendsapp.model
+
+data class MyFriend(
+    val nama : String,
+    val kelamin : String,
+    val email : String,
+    val telp: String,
+    val alamat: String
+)
