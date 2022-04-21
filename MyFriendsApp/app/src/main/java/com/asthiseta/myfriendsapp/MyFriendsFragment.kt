2c8603f4657b86affedcd9ef0ac811564a293311
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-22 00:54:55 +0700

    added dummy data

--- MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/MyFriendsFragment.kt
+++ MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/MyFriendsFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.asthiseta.myfriendsapp.adapter.MyFriendAdapter
+import com.asthiseta.myfriendsapp.model.MyFriend
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MyFriendsFragment : Fragment() {
 
     private var fab_btn: FloatingActionButton? = null
-
+    lateinit var listTeman: MutableList<MyFriend>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +30,23 @@ class MyFriendsFragment : Fragment() {
 
     }
 
+    private fun simulasiDataTeman() {
+        listTeman = ArrayList()
+
+        listTeman.add(
+            MyFriend(
+                "Muhammad", "Laki-laki",
+                "ade@gmail.com", "085719004268", "Bandung"
+            )
+        )
+        listTeman.add(
+            MyFriend(
+                "Al Harits", "Laki-laki",
+                "rifaldi@gmail.com", "081213416171", "Bandung"
+            )
+        )
+    }
+
     private fun initView() {
         fab_btn = activity?.findViewById(R.id.fabAddFriend)
 
@@ -34,6 +54,11 @@ class MyFriendsFragment : Fragment() {
             (activity as MainActivity).tampilMyFriendsAddFragment()
         }
 
+    }
+
+    private fun tampilTeman(){
+        //listMyFriends.layoutManager = LinearLayoutManager(activity)
+        //listMyFriends.adapter = MyFriendAdapter(activity!!, listTeman)
     }
 
     override fun onDestroy() {
