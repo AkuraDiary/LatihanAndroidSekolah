d42f29074dca1a8b6a9ccf8bb1b28b035807440e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-04-25 18:30:27 +0700

    changed data class into entity

--- MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/model/MyFriend.kt
+++ MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/model/MyFriend.kt
@@ -1,6 +1,12 @@
 package com.asthiseta.myfriendsapp.model
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
 data class MyFriend(
+    @PrimaryKey(autoGenerate = true)
+    val temanId : Int? = null,
     val nama : String,
     val kelamin : String,
     val email : String,
