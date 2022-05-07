4334b3cce34cf99f9bce5ffc1e8a2a87e1cbdfa9
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-05-07 16:52:39 +0700

    migrating

--- MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/database/AppDatabase.kt
+++ MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/database/AppDatabase.kt
@@ -1,9 +1,10 @@
 package com.asthiseta.myfriendsapp.database
 
-import android.arch.persistence.room.Database
-import android.arch.persistence.room.Room
-import android.arch.persistence.room.RoomDatabase
+
 import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.asthiseta.myfriendsapp.model.MyFriend
 
 @Database(entities = [MyFriend::class], version = 1)
--- MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/model/MyFriend.kt
+++ MyFriendsApp/app/src/main/java/com/asthiseta/myfriendsapp/model/MyFriend.kt
@@ -1,7 +1,8 @@
 package com.asthiseta.myfriendsapp.model
 
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.PrimaryKey
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 @Entity
 data class MyFriend(
